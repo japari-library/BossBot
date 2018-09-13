97bff111d03d19f897bc40c95ea3f61e72627157
Maid <maidslilyy@gmail.com>
2018-09-14 01:55:52 +0700

    Added Japari Library module

---
+++ NadekoBot.Core/Modules/Searches/Common/JapariLibraryAPIModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NadekoBot.Core.Modules.Searches.Common
+{
+
+
+    public class JapariLibraryAPIModel
+    {
+        public bool batchcomplete { get; set; }
+        public Continue @continue { get; set; }
+        public Query query { get; set; }
+        public class Continue
+        {
+            public int gsroffset { get; set; }
+            public string @continue { get; set; }
+        }
+
+        public class Revision
+        {
+            public int revid { get; set; }
+            public int parentid { get; set; }
+            public bool minor { get; set; }
+            public string user { get; set; }
+            public DateTime timestamp { get; set; }
+            public string comment { get; set; }
+        }
+
+        public class Page
+        {
+            public int pageid { get; set; }
+            public int ns { get; set; }
+            public string title { get; set; }
+            public int index { get; set; }
+            public string contentmodel { get; set; }
+            public string pagelanguage { get; set; }
+            public string pagelanguagehtmlcode { get; set; }
+            public string pagelanguagedir { get; set; }
+            public DateTime touched { get; set; }
+            public int lastrevid { get; set; }
+            public int length { get; set; }
+            public string fullurl { get; set; }
+            public string editurl { get; set; }
+            public string canonicalurl { get; set; }
+            public List<Revision> revisions { get; set; }
+        }
+
+        public class Query
+        {
+            public List<Page> pages { get; set; }
+        }
+    }
+}
---
+++ NadekoBot.Core/Modules/Searches/JapariLibrarySearches.cs
@@ -0,0 +1,52 @@
+﻿using Discord;
+using Discord.Commands;
+using NadekoBot.Extensions;
+using NadekoBot.Modules.Searches.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NadekoBot.Common.Attributes;
+using NadekoBot.Core.Common.Pokemon;
+using NadekoBot.Core.Services;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System;
+using NadekoBot.Core.Modules.Searches.Common;
+
+namespace NadekoBot.Modules.Searches
+{
+    public partial class Searches
+    {
+        public class JapariLibrarySearchCommand : NadekoSubmodule<SearchesService>
+        {
+            private static HttpClient http = new HttpClient(); // for reuse
+            [NadekoCommand, Usage, Description, Aliases]
+            public async Task JapariWiki([Remainder] string query = null)
+            {
+                query = query?.Trim();
+                if (string.IsNullOrWhiteSpace(query))
+                {
+                    await ReplyErrorLocalized("jl_wikisearch_query_empty").ConfigureAwait(false); return;
+                }
+                var msg = await Context.Channel.SendMessageAsync(GetText("jl_wikisearch_searching"));
+                string json;
+                try
+                {
+                    json = await http.GetStringAsync(String.Format("https://japari-library.com/w/api.php?action=query&formatversion=2&format=json&generator=search&gsrsearch={0}&gsrlimit=1&prop=info|revisions&inprop=url", Uri.EscapeDataString(query)));
+
+                } catch (System.Net.Http.HttpRequestException) {
+                    await msg.ModifyAsync(m => m.Content = GetText("jl_wikisearch_error"));
+                    return;
+                }
+                var data = JsonConvert.DeserializeObject<JapariLibraryAPIModel>(json);
+                if (data.query == null)
+                {
+                    await msg.ModifyAsync(m => m.Content = GetText("jl_wikisearch_article_not_found"));
+                    return;
+                }
+                await msg.ModifyAsync(m => m.Content = GetText("jl_wikisearch_success", query, data.query.pages[0].touched, data.query.pages[0].revisions[0].user, data.query.pages[0].lastrevid, data.query.pages[0].fullurl));
+            }
+        }
+    }
+}
