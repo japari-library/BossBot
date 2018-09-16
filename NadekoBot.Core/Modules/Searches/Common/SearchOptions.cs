531825e0a90656e78a2d2dd982fd7be382bbbb04
Giovanni Solaini <gsolaini@gmail.com>
2018-09-16 23:27:57 +0200

    Updated command strings to reflect new functionality, and made the "-n" option help text more clear

--- NadekoBot.Core/Modules/Searches/Common/SearchOptions.cs
+++ NadekoBot.Core/Modules/Searches/Common/SearchOptions.cs
@@ -6,7 +6,7 @@ namespace NadekoBot.Core.Modules.Searches.Common
     public class SearchOptions : INadekoCommandOptions
     {
         //Option to make search results unembedded
-        [Option('n', "unembed", Required = false, Default = false, HelpText = "Unembedded")]
+        [Option('n', "unembed", Required = false, Default = false, HelpText = "Unembed the link")]
         public bool IsUnembedded { get; set; } = false;
 
         public void NormalizeOptions() { /*nothing to normalise*/ }
