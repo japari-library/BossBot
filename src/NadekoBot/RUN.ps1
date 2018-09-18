cd95e8a253a779f6753b825e085ffd61c8b90033
Maidd <25988046+Maidd@users.noreply.github.com>
2018-09-18 20:20:50 +0700

    Added scheduled restart

--- src/NadekoBot/RUN.ps1
+++ src/NadekoBot/RUN.ps1
@@ -1,11 +1,17 @@
+$scheduledRestartTime = 5400 # time in seconds
+
 while ($true) {
 	$a = $a + 1
 	$process = Start-Process "dotnet.exe" -ArgumentList "run -c release" -PassThru 
-	$wait = Wait-Process -Id $process.Id 
-	echo "The bot crashed..."
+	$wait = Wait-Process -Id $process.Id -Timeout $scheduledRestartTime
+	
+	echo "The bot was shutdown..."
 	if ($process.ExitCode -eq 0) {
 		echo "Possibly a graceful shutdown, exiting.."
 		break
+	} elseif (Get-Process -Id $process.Id) {
+		echo "Process is running, killing!"
+		$process.CloseMainWindow()
 	} else {
 		echo "Process exited with $($process.ExitCode)"
 	}
