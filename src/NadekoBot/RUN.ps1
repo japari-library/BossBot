8126d6600c32ea44f96ad87f04fb86afb55b94f8
Maidd <25988046+Maidd@users.noreply.github.com>
2018-09-18 20:00:02 +0700

    Added additional feature to RUN.ps1

--- src/NadekoBot/RUN.ps1
+++ src/NadekoBot/RUN.ps1
@@ -1,5 +1,12 @@
-while ($true){
-	Start-Process "dotnet.exe" "run -c release"
-	$a = Get-Process nadekobot
-	$a.waitforexit()
+while ($true) {
+	$a = $a + 1
+	$process = Start-Process "dotnet.exe" -ArgumentList "run -c release" -PassThru 
+	$wait = Wait-Process -Id $process.Id 
+	echo "The bot crashed..."
+	if ($process.ExitCode -eq 0) {
+		echo "Possibly a graceful shutdown, exiting.."
+		break
+	} else {
+		echo "Process exited with $($process.ExitCode)"
+	}
 }
